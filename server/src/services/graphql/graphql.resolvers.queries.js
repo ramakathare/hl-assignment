f9e07cf5d31e50d10d4f459cc66cfb15ad1cb23b
Rama Kathare <rkathare@ezesoft.com>
2018-10-28 19:13:32 +0530

    limit and skip functionality implemented

--- server/src/services/graphql/graphql.resolvers.queries.js
+++ server/src/services/graphql/graphql.resolvers.queries.js
@@ -6,8 +6,16 @@ export default function(Query, Service, GetServiceName, FindServiceName) {
   });
   Object.assign(Query, {
     [`${FindServiceName}`]: (root, args, context) => {
-      return Service.find(Object.assign({}, context, { query: args })).then(result => {
-        return { total: result.length, items: result };
+      Object.assign(args, {$skip: args.skip });
+      Object.assign(args, {$limit: args.limit });
+      
+      delete args.skip;
+      delete args.limit;
+
+      let paginate = {default: Number.MAX_SAFE_INTEGER};
+
+      return Service.find(Object.assign({}, context, { query: args, paginate})).then(result => {
+        return { total: result.total, items: result.data };
       });
     },
   });
