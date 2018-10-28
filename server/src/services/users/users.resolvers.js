1b40b9df9568f30c49e44508da0730c1c0c7208d
Rama Kathare <rkathare@ezesoft.com>
2018-10-28 15:27:57 +0530

    added missing Users param

--- server/src/services/users/users.resolvers.js
+++ server/src/services/users/users.resolvers.js
@@ -1,10 +1,10 @@
 
-export default function (Profiles) {
+export default function (Users) {
 
   const usersResolvers = {
     Users: {
       profile: (user) => {
-        return Users.find({ query: { _id: user.owner }}).then(result=>result.data[0]);
+        return Users.find({ query: { _id: user.owner }}).then(result=> result[0] || result.data[0]);
       }
     }
   };
