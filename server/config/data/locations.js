7e708e549d11c9e6e2473f8d8e31e1e7fcaae5dc
Rama Kathare <rkathare@ezesoft.com>
2018-10-28 21:18:59 +0530

    fixed _id values

--- server/config/data/locations.js
+++ server/config/data/locations.js
@@ -1,5 +1,5 @@
 module.exports = [
-  { _id: "5b62B540BcFF044ffA169f60", active: true, title: "Berlin", country: "4X2Lug8JgTq6zNMWnZzwDN5" },
-  { _id: "507F191e810c19729De860eA", active: true, title: "Cologne", country: "4X2Lug8JgTq6zNMWnZzwDN5" },
-  { _id: "53DC98133bAb79000034E25a", active: true, title: "Stuttgart", country: "4X2Lug8JgTq6zNMWnZzwDN5" }
+  { _id: "uQMcIIyn0ZIKYwD88Gt3cDMH", active: true, title: "Berlin", country: "4X2Lug8JgTq6zNMWnZzwDN5" },
+  { _id: "y4vk8VX8hNXfgpsaV4zNCOgY", active: true, title: "Cologne", country: "4X2Lug8JgTq6zNMWnZzwDN5" },
+  { _id: "Y0xJCnwCCY0va4JmQKyTD8vW", active: true, title: "Stuttgart", country: "4X2Lug8JgTq6zNMWnZzwDN5" }
 ];
